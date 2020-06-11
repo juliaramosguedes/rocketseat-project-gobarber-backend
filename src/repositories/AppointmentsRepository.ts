e5a7b611879f671cb37f231e03717934469580de
Julia Ramos <juliaramosguedes@gmail.com>
2020-06-11 17:24:50 -0300

    feat(appointments) applying DTO

--- src/repositories/AppointmentsRepository.ts
+++ src/repositories/AppointmentsRepository.ts
@@ -1,6 +1,11 @@
 import Appointment from '../models/Appointment';
 import { isEqual } from 'date-fns';
 
+interface CreateAppointmentDTO {
+  provider: string;
+  date: Date;
+}
+
 class AppointmentsRepository {
   private appointments: Appointment[];
 
@@ -8,8 +13,8 @@ class AppointmentsRepository {
     this.appointments = [];
   }
 
-  public create(provider: string, date: Date): Appointment {
-    const appointment = new Appointment(provider, date);
+  public create({ provider, date }: CreateAppointmentDTO): Appointment {
+    const appointment = new Appointment({ provider, date });
     this.appointments.push(appointment);
     return appointment;
   }
