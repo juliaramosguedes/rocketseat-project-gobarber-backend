a6fb1896e6611a5a20d6dd365acd7eed83a7e507
Julia Ramos <juliaramosguedes@gmail.com>
2020-06-19 15:33:37 -0300

    feat(users) create route, service, model

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,7 +1,9 @@
 import { Router } from 'express';
 import appointmenstRouter from './appointments.routes';
+import usersRouter from './users.routes';
 const routes = Router();
 
 routes.use('/appointments', appointmenstRouter);
+routes.use('/users', usersRouter);
 
 export default routes;
